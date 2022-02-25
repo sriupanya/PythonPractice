f4a580afb97a2482e05e0774ae1c9e22fe101bf9
sriupanya <30802212+sriupanya@users.noreply.github.com>
2022-02-25 15:18:11 +0530

    Create tim_urbans_Life_in_weeks.py

---
+++ tim_urbans_Life_in_weeks.py
@@ -0,0 +1,9 @@
+age = input("What is your current age?")
+
+age=int(age)
+days= (90-age)*365
+weeks= (90-age)*52
+months= (90-age)*12
+
+print(f"you have {days} days, {weeks} weeks and {months} months left")
+
